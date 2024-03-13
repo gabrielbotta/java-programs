2a80a7ae678fa1f30755821b25e0721d8ea35096
gabriel <116321358+gabrielbotta@users.noreply.github.com>
2024-03-13 14:22:44 -0400

    Add files via upload

---
+++ atividade_fixacao/ButtonFrame.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atividade_fixacao;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ButtonFrame extends JFrame{
+    
+    private JButton plainJButton;
+    private JButton fancyJButton;
+}
