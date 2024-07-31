685fae7c3ada2efa87b0c661c99425f92d51edf4
gabriel <116321358+gabrielbotta@users.noreply.github.com>
2024-07-31 13:57:27 -0400

    Add files via upload

---
+++ flowlayoutframe/FlowLayoutDemo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package flowlayoutframe;
+import javax.swing.JFrame;
+/**
+ *
+ * @author Aluno
+ */
+public class FlowLayoutDemo {
+    public static void main(String[] args){
+        FlowLayoutFrame flowLayoutFrame = new FlowLayoutFrame();
+        flowLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        flowLayoutFrame.setSize(300, 75);
+        flowLayoutFrame.setVisible(true);
+    }
+}
