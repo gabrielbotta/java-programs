32d29e6ad1e1ee09330f4c94b9f26016b4bac585
gabriel <116321358+gabrielbotta@users.noreply.github.com>
2024-07-31 14:42:07 -0400

    Add files via upload

---
+++ borderlayoutframe/BorderLayoutDemo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package borderlayoutframe;
+import javax.swing.JFrame;
+/**
+ *
+ * @author Aluno
+ */
+public class BorderLayoutDemo {
+    public static void main(String[] args){
+        BorderLayoutFrame borderLayoutFrame = new BorderLayoutFrame();
+        borderLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        borderLayoutFrame.setSize(300,200);
+        borderLayoutFrame.setVisible(true);
+    }
+}
