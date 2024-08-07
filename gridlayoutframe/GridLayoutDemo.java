16c752e63205dbebe84ff0a34a2ab837e31363d4
gabriel <116321358+gabrielbotta@users.noreply.github.com>
2024-08-07 14:29:02 -0400

    Add files via upload

---
+++ gridlayoutframe/GridLayoutDemo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gridlayoutframe;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Aluno
+ */
+public class GridLayoutDemo {
+    
+    public static void main(String[] args)
+    {
+        GridLayoutFrame gridLayoutFrame = new GridLayoutFrame();
+        gridLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gridLayoutFrame.setSize(300,200);
+        gridLayoutFrame.setVisible(true);
+    }
+    
+}
