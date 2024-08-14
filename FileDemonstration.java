db277eeea2dd5002d71366c661ee9adc56eeae7a
gabriel <116321358+gabrielbotta@users.noreply.github.com>
2024-08-14 13:51:15 -0400

    Add files via upload

---
+++ FileDemonstration.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package filedemonstration;
+
+/**
+ *
+ * @author Aluno
+ */
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileDemonstration {
+
+    
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("nome do diretorio: ");
+        analyzePath(input.nextLine());
+               
+        
+    }
+    
+    public static void analyzePath(String path){
+        File name = new File(path);
+        if(name.exists()){
+            System.out.printf("%s%s\n%s\n%s\n%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s",
+                    name.getName(), "exists",
+                    (name.isFile()?"is a file":"is not a file"),
+                    (name.isDirectory()?"is a directory": "is not absolute path"), "Last modified: ",
+                    name.lastModified(), "Lenght: ", name.length(),
+                    "path: ", name.getPath(), "Parent: ", name.getParent());
+            
+            if(name.isDirectory()){
+                String[] directory = name.list();
+                System.out.println("\n\nDirectory contents:\n");
+                
+                for(String directoryName : directory){
+                    System.out.println(directoryName);
+                }
+            }
+            else{
+                System.out.printf("%s%s", path, "does not exist.");
+            }
+            
+                
+        
+            
+            }
+        }
+    }
+    
+
